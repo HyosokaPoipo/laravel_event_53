0201385f679789daad9dbfff3336a5b23bbce885
Maryadi <maryadi@astrnt.co>
2016-09-27 13:53:53 +0700

    Registering event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,10 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\SomeEvent' => [
-            'App\Listeners\EventListener',
+        'App\Events\ActionDone' => [
+            'App\Listeners\ADListener',
+            //'App\Listeners\OtherThingstoDo',
+            //'App\Listeners\Others',
         ],
     ];
 
